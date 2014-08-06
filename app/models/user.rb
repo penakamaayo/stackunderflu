da9d7040480ebebe22938eac68844773d79565f5
james penya <penakamaayo@gmail.com>
2014-08-06 10:35:39 +0800

    added before_save :encrypt_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   attr_accessor :password
   validates_confirmation_of :password
+  before_save :encrypt_password
 
   def encrypt_password
     self.password_salt = BCrypt::Engine.generate_salt
