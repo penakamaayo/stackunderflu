db6d908346fec452ee5e696f2194351cb2a395f1
james penya <penakamaayo@gmail.com>
2014-08-06 10:22:46 +0800

    added hash function

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ActiveRecord::Base
+  attr_accessor :password
+  validates_confirmation_of :password
+
+  def encrypt_password
+    self.password_salt = BCrypt::Engine.generate_salt
+    self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+  end
+
+  def self.authenticate(email, password)
+
+  end
 end
