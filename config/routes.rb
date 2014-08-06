bdd029675655916516c77062363de64ed8e7e1fa
james penya <penakamaayo@gmail.com>
2014-08-06 10:36:46 +0800

    added new resource route users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  resources :users
+  
   get 'home/index'
 
   get 'users/new'
@@ -7,7 +10,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
