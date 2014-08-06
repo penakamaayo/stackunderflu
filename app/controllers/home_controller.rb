66f6a8668a852b1bb66d9aee424a3078174444fb
james penya <penakamaayo@gmail.com>
2014-08-06 13:11:58 +0800

    added login filter

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,17 @@
 class HomeController < ApplicationController
+  before_action :require_login
+  
   def index
+
+  end
+
+  private
+
+  def require_login
     if current_user
-      
+
     else
+      # flash[:error] = "You must be logged in to access this section"
       redirect_to sessions_new_url
     end
   end
