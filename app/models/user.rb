00281d8ba0b510da4428f65a71954788dee56fc7
james penya <penakamaayo@gmail.com>
2014-08-06 13:52:49 +0800

    updates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  validates :first_name, :presence => true
+  validates :first_name, :last_name, :email, :presence => true
   validates :email, :presence => true, uniqueness: {message: "Email already taken."}
   
   attr_accessor :password
