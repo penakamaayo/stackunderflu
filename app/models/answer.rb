7f3e54449360ad8a1f43dbbc37306534448ea559
james penya <penakamaayo@gmail.com>
2014-08-08 15:21:53 +0800

    replace answer model with reply

--- app/models/answer.rb
+++
@@ -1,5 +0,0 @@
-class Answer < ActiveRecord::Base
-  belongs_to :response, :polymorphic => true
-  belongs_to :user
-  has_many :comments, :dependent => :destroy, :as => :response
-end
--- db/migrate/20140808071402_create_replies.rb
+++ db/migrate/20140808071402_create_replies.rb
@@ -1,6 +1,6 @@
-class CreateAnswers < ActiveRecord::Migration
+class CreateReplies < ActiveRecord::Migration
   def change
-    create_table :answers do |t|
+    create_table :replies do |t|
       t.text :body
       t.references :user
       t.references :response, polymorphic: true
