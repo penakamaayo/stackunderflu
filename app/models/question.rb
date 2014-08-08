76f92c35221714714b9c61ee47d2c1f3d98c4890
james penya <penakamaayo@gmail.com>
2014-08-08 15:29:48 +0800

    update relation

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,4 @@
 class Question < ActiveRecord::Base
   belongs_to :user
-  has_many :answers, :dependent => :destroy, :as => :response
+  has_many :answers, :class_name => "Reply", :dependent => :destroy, :as => :response
 end
--- app/models/reply.rb
+++ app/models/reply.rb
@@ -1,2 +1,5 @@
 class Reply < ActiveRecord::Base
+ belongs_to :response, :polymorphic => true
+ belongs_to :user
+ has_many :comments, :class_name => "Reply", :dependent => :destroy, :as => :response
 end
