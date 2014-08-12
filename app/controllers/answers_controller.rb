24ec7beda4dfc1b38103fd5d2e35afe574d6961f
james penya <penakamaayo@gmail.com>
2014-08-12 10:19:27 +0800

    update create action

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -2,7 +2,17 @@ class AnswersController < ApplicationController
   def new
   end
 
+  def show
+  end
+
   def create
+    @question = Question.find(params[:question_id])
+    @answer = @question.answers.create(answer_params)  
+
+    respond_to do |format|
+      format.html { redirect_to question_path(@question) }
+      format.js 
+    end
   end
 
   def update
@@ -10,4 +20,10 @@ class AnswersController < ApplicationController
 
   def destroy
   end
+
+  private
+  def answer_params
+    params.require(:reply).permit(:user_id, :body)
+  end
+
 end
