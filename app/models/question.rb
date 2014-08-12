5d87c82341b120c3b1d3cb49b4befe994a32b305
james penya <penakamaayo@gmail.com>
2014-08-12 14:22:18 +0800

    added relation question-votes

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,5 @@
 class Question < ActiveRecord::Base
   belongs_to :user
   has_many :answers, :class_name => "Reply", :dependent => :destroy, :as => :response
+  has_many :votes, :as => :voteable
 end
