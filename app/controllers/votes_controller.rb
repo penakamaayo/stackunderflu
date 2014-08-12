94c2849b6839f510e892c544d9e6bc234d8448f7
james penya <penakamaayo@gmail.com>
2014-08-12 16:00:04 +0800

    updates

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,2 +1,33 @@
 class VotesController < ApplicationController
+  def create
+    @question = Question.find(params[:question_id])
+
+    voteable = @question
+
+    voteable.votes.create(:user_id => self.current_user.id, :vote_count => params[:vote_count])
+
+    @vid = voteable.id
+
+    respond_to do |format|
+      format.html { redirect_to question_path(@question) }
+      format.js
+    end
+  end
+
+  def destroy
+    vote = Vote.find(params[:id])
+    if vote.voteable_type ==  'Question' 
+      @question = vote.voteable
+    else
+      @answer = vote.voteable
+      @question = @answer.response
+      @vid = @answer.id
+    end
+    vote.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(questions_path) }
+      format.js 
+    end
+  end
 end
