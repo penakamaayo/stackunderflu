1e20feb042f2c7a5dc3646720e7df0dfe3f0e03e
james penya <penakamaayo@gmail.com>
2014-08-12 11:11:21 +0800

    added answer_id to replies

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140812025747) do
+ActiveRecord::Schema.define(version: 20140812030759) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20140812025747) do
     t.string   "response_type"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "answer_id"
   end
 
   create_table "users", force: true do |t|
