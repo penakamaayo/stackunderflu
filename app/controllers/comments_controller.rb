46a00c29d5f60a0827af7aab28defd7c31759d0a
james penya <penakamaayo@gmail.com>
2014-08-12 10:29:25 +0800

    update create action on comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,11 +3,27 @@ class CommentsController < ApplicationController
   end
 
   def create
+    @answer = Reply.find(params[:answer_id])
+
+    @comment = @answer.comments.create(comment_params)
+
+    @id = params[:reply_id]
+
+    respond_to do |format|
+      format.html { redirect_to question_path(@question) }
+      format.js 
+    end
   end
 
   def update
   end
 
-  def destory
+  def destroy
+  end
+
+  private
+
+  def comment_params
+    params.require(:reply).permit(:user_id, :body, :answer_id)
   end
 end
