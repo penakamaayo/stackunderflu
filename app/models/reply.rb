13138d9fe627ee7b5a257a8f85e750572883ada1
james penya <penakamaayo@gmail.com>
2014-08-13 10:33:03 +0800

    update vote func

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -1,21 +1,23 @@
 class Reply < ActiveRecord::Base
- belongs_to :repliable, :polymorphic => true
- belongs_to :user
- belongs_to :answers, :class_name => "Reply"
- has_many :comments, :foreign_key => "answer_id", :class_name => "Reply", :dependent => :destroy, :as => :repliable
- has_many :votes, :as => :voteable, :dependent => :destroy
+  belongs_to :repliable, :polymorphic => true
+  belongs_to :user
+  belongs_to :answers, :class_name => "Reply"
+  has_many :comments, :foreign_key => "answer_id", :class_name => "Reply", :dependent => :destroy, :as => :repliable
+  has_many :votes, :as => :voteable, :dependent => :destroy
 
   def total_votes
-    self.votes.sum(&:vote_value)
-
+    sum = 0
+    self.votes.each do |vote|
+      sum = sum + vote.vote_value
+    end
+    sum
   end
 
-  def user_vote(current_user)
-    vote_instance  = self.votes.where(
-        :user_id => current_user).
-    first
-    if vote_instance != nil
-      vote_instance.vote_value
+  def users_existing_vote(current_user)
+    user_vote = self.votes.where(:user_id => current_user.id).first
+
+    if user_vote
+      user_vote.vote_value   
     else
       0
     end
