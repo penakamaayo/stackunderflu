759bf84e982ced703c58164d4aba8c721f02b599
james penya <penakamaayo@gmail.com>
2014-08-13 17:00:45 +0800

    used concerns include voteable

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,21 +3,5 @@ class Question < ActiveRecord::Base
   has_many :answers, :class_name => "Reply", :dependent => :destroy, :as => :repliable
   has_many :votes, :as => :voteable, :dependent => :destroy
 
-  def total_votes
-    sum = 0
-    self.votes.each do |vote|
-      sum = sum + vote.vote_value
-    end
-    sum
-  end
-
-  def users_existing_vote(current_user)
-    user_vote = self.votes.where(:user_id => current_user.id).first
-
-    if user_vote
-      user_vote.vote_value   
-    else
-      0
-    end
-  end
+  include Voteable
 end
--- app/models/reply.rb
+++ app/models/reply.rb
@@ -4,22 +4,6 @@ class Reply < ActiveRecord::Base
   belongs_to :answers, :class_name => "Reply"
   has_many :comments, :foreign_key => "answer_id", :class_name => "Reply", :dependent => :destroy, :as => :repliable
   has_many :votes, :as => :voteable, :dependent => :destroy
-
-  def total_votes
-    sum = 0
-    self.votes.each do |vote|
-      sum = sum + vote.vote_value
-    end
-    sum
-  end
-
-  def users_existing_vote(current_user)
-    user_vote = self.votes.where(:user_id => current_user.id,:voteable_type => "Reply").first
-
-    if user_vote
-      user_vote.vote_value   
-    else
-      0
-    end
-  end
+  
+  include Voteable
 end
