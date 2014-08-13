e1b0295ffe6ce078a100816a30706cc4a4bb5f39
james penya <penakamaayo@gmail.com>
2014-08-13 15:28:46 +0800

    refactor

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,50 +1,32 @@
 class VotesController < ApplicationController
   def create
+    @question = Question.find(params[:question_id])
     type = params[:voteable_type]
 
     if type == 'Question'
-      @question = Question.find(params[:question_id])
-      voteable = @question
-      voteable.votes.create(:user_id => self.current_user.id, :vote_value => params[:vote_value])
-      flash[:notice] = "Done"
-
-      respond_to do |format|
-        format.html { redirect_to question_path(@question) }
-        format.js
-      end
+      @question.votes.create(:user_id => self.current_user.id, :vote_value => params[:vote_value])
 
     elsif type == 'Answer'
-      @question = Question.find(params[:question_id])
       @answer = Reply.find(params[:answer_id])
-
       @answer.votes.create(:user_id => self.current_user.id, :vote_value => params[:vote_value])
 
-      flash[:notice] = "Done"
-
-      respond_to do |format|
-        format.html { redirect_to question_path(@question) }
-        format.js
-      end
-
     else
-      @question = Question.find(params[:question_id])
       @comment = Reply.find(params[:comment_id])
-
       @comment.votes.create(:user_id => self.current_user.id,:vote_value => params[:vote_value])
+    end
 
-      flash[:notice] = "Done"
+    flash[:notice] = "Done"
 
-      respond_to do |format|
-        format.html { redirect_to question_path(@question) }
-        format.js
-      end
+    respond_to do |format|
+      format.html { redirect_to question_path(@question) }
+      format.js
     end
   end
 
   def destroy
     @question = Question.find(params[:question_id])
     vote = Vote.find(params[:id])
-    
+
     respond_to do |format|
       format.html { redirect_to(question_path(@question)) }
       format.js 
