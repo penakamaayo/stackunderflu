c717f4d155f57aaf8eee7d5263c551f364eec3f1
james penya <penakamaayo@gmail.com>
2014-08-18 12:09:17 +0800

    update total_votes func

--- app/models/concerns/voteable.rb
+++ app/models/concerns/voteable.rb
@@ -1,7 +1,18 @@
 module Voteable
   extend ActiveSupport::Concern
   def total_votes
-    self.votes.inject(0){ |sum, vote| sum += vote.vote_value }
+    # self.votes.inject(0){ |sum, vote| sum += vote.vote_value }
+    sum = 0
+    self.votes.each do |vote|
+      if vote.vote_value == 1 || vote.vote_value == 0 || vote.vote_value == -1
+        sum += vote.vote_value
+      elsif vote.vote_value > 1
+        sum += 1
+      else
+        sum += -1
+      end
+    end
+    sum
   end
 
   def upvoted_by? current_user
