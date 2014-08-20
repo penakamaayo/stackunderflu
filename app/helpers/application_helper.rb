3dff8c9860161f047aed7afc2bc64b2c4ad46ee3
james penya <penakamaayo@gmail.com>
2014-08-20 13:04:29 +0800

    done user mention on answers&comments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,30 @@
 module ApplicationHelper
+  def auto_link text
+    text_arr = text.split(" ")
+
+    result = []
+
+    text_arr.each do |word|
+      if word[0] == '@'
+        username_link = convert_to_link word
+        result << username_link
+      else
+        result << word
+      end
+    end
+    result.join(" ")
+  end
+
+
+  private
+  def convert_to_link username
+    username.slice!(0)
+    if User.where(:username => username).first
+      link_to "@"<<username, user_path(username).html_safe
+      # link = ("<a href=\"/users/#{username}\">" + "@"+ username + "</a>")
+      # link.html_safe
+    else
+      username
+    end
+  end
 end
