b43d2bb3074e0e5da20ac956896836ae5499941d
james penya <penakamaayo@gmail.com>
2014-08-20 14:20:03 +0800

    added new model tag

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140819091248) do
+ActiveRecord::Schema.define(version: 20140820061900) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,12 @@ ActiveRecord::Schema.define(version: 20140819091248) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "answer_id"
+  end
+
+  create_table "tags", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
