6ab462246ba3bbe62bd2ee8e520d4ce3ad35d8ac
james penya <penakamaayo@gmail.com>
2014-08-20 15:01:23 +0800

    added question_tag model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140820061900) do
+ActiveRecord::Schema.define(version: 20140820065224) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "question_tags", force: true do |t|
+    t.integer  "question_id"
+    t.integer  "tag_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "questions", force: true do |t|
     t.string   "title"
