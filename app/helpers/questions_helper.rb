32842d44786d788f25f5ce3d7e4fa868ab19e6ef
james penya <penakamaayo@gmail.com>
2014-08-21 14:53:10 +0800

    update expression indention

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -3,17 +3,14 @@ module QuestionsHelper
     tag_names = tags.split(", ")
 
     result = []
-
     tag_names.each do |tag|
       result << convert_to_link(tag)
-    end
-    
+    end    
     result.join(", ")
   end
 
-
   private
-  def convert_to_link tag
-    link_to tag, tag_path(tag).html_safe
-  end
+    def convert_to_link tag
+      link_to tag, tag_path(tag).html_safe
+    end
 end
