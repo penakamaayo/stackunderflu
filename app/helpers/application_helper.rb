43d202eeb6feb5c766a41d05632def530134176f
james penya <penakamaayo@gmail.com>
2014-08-21 14:52:34 +0800

    update expression indention

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,6 @@ module ApplicationHelper
     text_arr = text.split(" ")
 
     result = []
-
     text_arr.each do |word|
       if word[0] == '@'
         username_link = convert_to_link word
@@ -17,14 +16,12 @@ module ApplicationHelper
 
 
   private
-  def convert_to_link username
-    username.slice!(0)
-    if User.where(:username => username).first
-      link_to "@"<<username, user_path(username).html_safe
-      # link = ("<a href=\"/users/#{username}\">" + "@"+ username + "</a>")
-      # link.html_safe
-    else
-      username
+    def convert_to_link username
+      username.slice!(0)
+      if User.where(:username => username).first
+        link_to "@" << username, user_path(username).html_safe
+      else
+        username
+      end
     end
-  end
 end
