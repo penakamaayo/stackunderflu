df930643292c51010f8d95e2e348d67b73ddd8fb
james penya <penakamaayo@gmail.com>
2014-08-26 08:46:36 +0800

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,8 +12,7 @@ class User < ActiveRecord::Base
   validates :email, :presence => true, uniqueness: {:message => "Email already taken."}
   validates :username, :presence => true, uniqueness: {:message => "Username already taken."}
 
-  # //lay self
-  def self.encrypt_password
+  def encrypt_password
     self.password_salt = BCrypt::Engine.generate_salt
     self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
   end
